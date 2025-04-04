644b58fe0b3382d247155cf961bba48475ac7ef5
Tanisha <tanisha.bamne1980@gmail.com>
2025-04-04 23:51:11 +0530

    update

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def home(request):
     x="<h1> hello world </h1>"
-    return Http
+    return HttpResponse(x)
+def about (request):
+    data={'name':True,'age':False,'quals':None}
+    return JsonResponse(data)
+def index(request):
+    return render(request,'index.html')
+def new(request):
+    return redirect('https://www.google.com')
+
+
+def index(request):
+    x={}
+    data=[{'name':'tanisha','city':'bhopal'}]
+    x['key1']=data
+    return render(request,'index.html',x)
+
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path 
+from app import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('home/',views.home,name='home')
+    path('home/',views.home,name='home'),
+    path('about/', views.about, name='about'),
+    path('index/',views.index, name='index'),
+    path('new/', views.new, name='new'),
+    
+
 ]
